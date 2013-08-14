051dcc9622e05dfd96dfdbf6ca810b398f94f1bc
Eric Sims <EricSimsM@gmail.com>
2013-08-14 18:32:22 -0400

    fixed acc.ino

--- acc/acc.ino
+++ acc/acc.ino
@@ -1,17 +1,14 @@
 #include <Wire.h> // I2C library, gyroscope
 
 // Accelerometer ADXL345
-const int ACCADDRESS = 0x53;    //ADXL345 ACC address
+#define ACC 0x53    //ADXL345 ACC address
 #define A_TO_READ (6)        //num of bytes we are going to read each time (two bytes for each axis)
-
-
 // Gyroscope ITG3200 
 #define GYRO 0x69 // gyro address, binary = 11101001 when AD0 is connected to Vcc (see schematics of your breakout board)
 #define G_SMPLRT_DIV 0x15
 #define G_DLPF_FS 0x16
 #define G_INT_CFG 0x17
 #define G_PWR_MGM 0x3E
-
 #define G_TO_READ 8 // 2 bytes for each axis x, y, z
 
 // offsets are chip specific. 
@@ -23,9 +20,9 @@ char str[512];
 
 void initAcc() {
   //Turning on the ADXL345
-  writeTo(ACCADDRESS, 0x2D, 0);      
-  writeTo(ACCADDRESS, 0x2D, 16);
-  writeTo(ACCADDRESS, 0x2D, 8);
+  writeTo(ACC, 0x2D, 0);      
+  writeTo(ACC, 0x2D, 16);
+  writeTo(ACC, 0x2D, 8);
   //by default the device is in +-2g range reading
 }
 
@@ -33,7 +30,7 @@ void getAccelerometerData(int * result) {
   int regAddress = 0x32;    //first axis-acceleration-data register on the ADXL345
   byte buff[A_TO_READ];
   
-  readFrom(ACCADDRESS, regAddress, A_TO_READ, buff); //read the acceleration data from the ADXL345
+  readFrom(ACC, regAddress, A_TO_READ, buff); //read the acceleration data from the ADXL345
   
   //each axis reading comes in 10 bit resolution, ie 2 bytes.  Least Significat Byte first!!
   //thus we are converting both bytes in to one int
